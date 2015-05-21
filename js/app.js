d138a52d6501356e5890598245527e62a8d47a5d
Velio <v.golev@yahoo.com>
2015-05-21 13:25:38 +0300

    page routing and ng-click/hide for the login/register

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
- 
+"use strict";
+
+var socialNetwork = angular
+	.module( "socialNetwork", [ "ngResource", "ngRoute" ] )
+	.config( function( $routeProvider ) {
+
+		$routeProvider
+			.when( "/", {
+				templateUrl: "/partials/login-register.html"
+			})
+			.otherwise({redirectTo: "/"});
+	});
